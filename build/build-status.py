41e69550b638db8137439fd877ccb93a2ebfbcb7
Youngera <chenzhiyong.chen@gmail.com>
2014-05-30 13:28:39 +0800

    add the whole project into the github

---
+++ build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/ns3.18-first-debug', 'build/scratch/ns3.18-second-debug', 'build/scratch/subdir/ns3.18-subdir-debug', 'build/scratch/ns3.18-test-debug', 'build/scratch/ns3.18-FatTreeTopology-debug', 'build/scratch/ns3.18-scratch-simulator-debug', 'build/scratch/ns3.18-myfirst-debug', 'build/scratch/ns3.18-NewFatTree-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
